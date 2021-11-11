d2fdf42c9117c92f9c4c3c50bbf3e67c20209c13
Lucas-Guimaraes <lucasthirdg@gmail.com>
2021-11-11 15:44:05 -0800

    Time: 116 ms (55.47%), Space: 49.2 MB (32.94%) - LeetHub

---
+++ rotate-array/rotate-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    let n = nums.length;
+    if (k % n) k = k % n;
+    nums.splice(0, 0, ...nums.splice(n-k))
+    
+};
