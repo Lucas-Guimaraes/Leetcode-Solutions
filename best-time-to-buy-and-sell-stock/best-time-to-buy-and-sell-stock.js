0c30533b08711994d8e675594439e5ef48e8f7f9
Lucas-Guimaraes <lucasthirdg@gmail.com>
2021-11-18 14:20:22 -0800

    Time: 104 ms (44.60%), Space: 49 MB (20.67%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    min = Infinity
+    profit = 0
+    for (let i = 0; i < prices.length; i++)
+    {
+        
+        if (prices[i] < min) {min = prices[i]}
+        else if (prices[i] - min > profit) {profit = prices[i] - min}
+    }
+    return profit
+};
