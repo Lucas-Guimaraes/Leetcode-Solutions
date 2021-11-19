720dc992d5aef589a5f20a77da64d14f4a32b8f8
Lucas-Guimaraes <lucasthirdg@gmail.com>
2021-11-19 15:35:25 -0800

    Time: 72 ms (72.35%), Space: 38.8 MB (14.57%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    n1 = 0, n2 = 1, count = 0, step = 0
+    while (count < n)
+    {
+        step = n1+n2
+        n1 = n2
+        n2 = step
+        count++
+    }
+    return step
+};
