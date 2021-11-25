8b212a0048f757a6e8ab8a3740c92d327dae03e2
Lucas-Guimaraes <lucasthirdg@gmail.com>
2021-11-24 19:32:14 -0800

    Time: 454 ms (16.07%), Space: 38.5 MB (98.21%) - LeetHub

---
+++ count-square-sum-triples/count-square-sum-triples.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countTriples = function(n) {
+    count = 0
+    if (n <= 2) {return count}
+    for (let a = 1; a <= n; a++) {
+        for (let b = 1; b <= n; b++) {
+            for (let c = 1; c <= n; c++) {
+                if (a*a + b*b === c*c) {count++}
+            }
+        }
+    }
+    return count
+};
