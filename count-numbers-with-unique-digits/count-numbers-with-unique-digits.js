c43c72a7b3b6989b22c38bc11223327c53c72e7f
Lucas-Guimaraes <lucasthirdg@gmail.com>
2021-12-03 16:11:14 -0800

    Time: 72 ms (72.55%), Space: 38.8 MB (31.37%) - LeetHub

---
+++ count-numbers-with-unique-digits/count-numbers-with-unique-digits.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countNumbersWithUniqueDigits = function(n) {
+    arr = [1, 10, 91, 739, 5275, 32491, 168571, 712891, 2345851]
+    return arr[n]
+};
