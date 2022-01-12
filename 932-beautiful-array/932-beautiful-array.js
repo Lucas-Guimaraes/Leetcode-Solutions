7f19479a072c03007f554e08c4ec3fe7472697ab
Lucas-Guimaraes <lucasthirdg@gmail.com>
2022-01-12 14:01:08 -0800

    Time: 132 ms (17.50%), Space: 41 MB (77.50%) - LeetHub

---
+++ 932-beautiful-array/932-beautiful-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var beautifulArray = function(n) {
+    let res = [1]
+    while (res.length < n)
+    {
+        let tmp = []
+        for (const x of res) {if (x * 2 - 1 <= n) tmp.push(x * 2 - 1)}
+        for (const x of res) {if (x * 2 <= n) tmp.push(x * 2)}
+        res = tmp
+    }
+    return res
+};
